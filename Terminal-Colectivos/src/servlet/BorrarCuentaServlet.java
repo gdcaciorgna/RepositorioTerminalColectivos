22953a6b5cb3c9d4eb6223f56fbd8b104a47dac8
Gerardo <gdcaciorgna@hotmail.com>
2019-06-26 20:25:36 -0300

    26/06/2019

--- Terminal-Colectivos/src/servlet/BorrarCuentaServlet.java
+++ Terminal-Colectivos/src/servlet/BorrarCuentaServlet.java
@@ -7,17 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import data.DataUsuario;
+import entities.Usuario;
+
 /**
- * Servlet implementation class DeleteAccountServlet
+ * Servlet implementation class BorrarCuentaServlet
  */
-@WebServlet("/DeleteAccountServlet")
-public class DeleteAccountServlet extends HttpServlet {
+@WebServlet("/BorrarCuentaServlet")
+public class BorrarCuentaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DeleteAccountServlet() {
+    public BorrarCuentaServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,6 +31,43 @@ public class DeleteAccountServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Usuario usuarioEncontrado;
+		DataUsuario dusu = new DataUsuario();
+		boolean testIgualdad, r;
+		
+		
+		String txtusu;
+		String txtpass1 = request.getParameter("txtpass1");
+		String txtpass2 = request.getParameter("txtpass2");
+		txtusu = (String) request.getParameter("txtusu");
+		
+		usuarioEncontrado = dusu.getByUsuario(txtusu);
+		
+		testIgualdad = validarIgualdadContraseñas(txtpass1, txtpass2);
+		if (testIgualdad) 
+		{
+			request.getSession().setAttribute("error", "Las contraseñas SI coinciden");
+		}
+		else 
+		{
+			request.getSession().setAttribute("error", "Las contraseñas no coinciden.");
+		}
+		
+		 response.sendRedirect("asdasdas.jsp");
+	
+		
+
+	
+	
+	}
+ 
+	
+
+	private boolean validarIgualdadContraseñas(String txtpass1, String txtpass2) {
+		if(txtpass1.equals(txtpass2))
+		return true;
+		else return false;
 	}
 
 	/**
