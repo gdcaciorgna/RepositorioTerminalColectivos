8e031dbf20d81c777761d20894893ee7b2d83799
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2019-06-27 14:38:48 -0300

    Actualizamos base de datos y driver de mysql

--- Terminal-Colectivos/db/db_terminal_colectivos.sql
+++ Terminal-Colectivos/db/db_terminal_colectivos.sql
@@ -1,326 +1,319 @@
-CREATE DATABASE  IF NOT EXISTS `db_terminal_colectivos` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `db_terminal_colectivos`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: db_terminal_colectivos
--- ------------------------------------------------------
--- Server version	5.0.24-community-nt
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-06-2019 a las 17:43:01
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Not dumping tablespaces as no INFORMATION_SCHEMA.FILES table on this server
+-- Base de datos: `db_terminal_colectivos`
 --
 
+-- --------------------------------------------------------
+
 --
--- Table structure for table `ciudad`
+-- Estructura de tabla para la tabla `ciudad`
 --
 
-DROP TABLE IF EXISTS `ciudad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ciudad` (
   `cod_postal` int(11) NOT NULL,
-  `nombre` varchar(45) default NULL,
-  PRIMARY KEY  (`cod_postal`)
+  `nombre` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ciudad`
+-- Volcado de datos para la tabla `ciudad`
 --
 
-LOCK TABLES `ciudad` WRITE;
-/*!40000 ALTER TABLE `ciudad` DISABLE KEYS */;
-INSERT INTO `ciudad` VALUES (2000,'Rosario'),(2600,'Venado Tuerto');
-/*!40000 ALTER TABLE `ciudad` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `ciudad` (`cod_postal`, `nombre`) VALUES
+(2000, 'Rosario'),
+(2600, 'Venado Tuerto');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `colectivo`
+-- Estructura de tabla para la tabla `colectivo`
 --
 
-DROP TABLE IF EXISTS `colectivo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `colectivo` (
   `patente` varchar(45) NOT NULL,
-  `capacidad` int(11) default NULL,
-  `estado` varchar(45) default NULL,
-  PRIMARY KEY  (`patente`)
+  `capacidad` int(11) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `colectivo`
+-- Estructura de tabla para la tabla `compania_tarjeta`
 --
 
-LOCK TABLES `colectivo` WRITE;
-/*!40000 ALTER TABLE `colectivo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `colectivo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `compania_tarjeta`
---
-
-DROP TABLE IF EXISTS `compania_tarjeta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `compania_tarjeta` (
-  `cod_compania` int(11) NOT NULL auto_increment,
-  `nombre` varchar(45) default NULL,
-  PRIMARY KEY  (`cod_compania`)
+  `cod_compania` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `compania_tarjeta`
+-- Estructura de tabla para la tabla `escala`
 --
 
-LOCK TABLES `compania_tarjeta` WRITE;
-/*!40000 ALTER TABLE `compania_tarjeta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `compania_tarjeta` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `escala`
---
-
-DROP TABLE IF EXISTS `escala`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `escala` (
   `cod_ruta` int(11) NOT NULL,
   `orden` int(11) NOT NULL,
-  `cod_terminal` int(11) default NULL,
-  PRIMARY KEY  (`cod_ruta`,`orden`),
-  KEY `escala_terminal_idx` (`cod_terminal`),
-  CONSTRAINT `escala_ruta` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `escala_terminal` FOREIGN KEY (`cod_terminal`) REFERENCES `terminal` (`cod_terminal`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `cod_terminal` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `escala`
+-- Estructura de tabla para la tabla `pasajero`
 --
 
-LOCK TABLES `escala` WRITE;
-/*!40000 ALTER TABLE `escala` DISABLE KEYS */;
-/*!40000 ALTER TABLE `escala` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pasajero`
---
-
-DROP TABLE IF EXISTS `pasajero`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pasajero` (
   `dni` int(11) NOT NULL,
-  `nombre` varchar(45) default NULL,
-  `apellido` varchar(45) default NULL,
-  PRIMARY KEY  (`dni`)
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `pasajero`
+-- Estructura de tabla para la tabla `pasajero_reserva`
 --
 
-LOCK TABLES `pasajero` WRITE;
-/*!40000 ALTER TABLE `pasajero` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pasajero` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pasajero_reserva`
---
-
-DROP TABLE IF EXISTS `pasajero_reserva`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pasajero_reserva` (
   `dni` int(11) NOT NULL,
   `cod_ruta` int(11) NOT NULL,
-  `fecha_res` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `fecha_res` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `usuario` varchar(45) NOT NULL,
-  `asiento` int(11) default NULL,
-  PRIMARY KEY  (`dni`,`cod_ruta`,`fecha_res`,`usuario`),
-  KEY `pasajero_reserva_usuario_idx` (`usuario`),
-  KEY `pasajero_reserva_reserva_idx` (`fecha_res`),
-  KEY `pasajero_reserva_ruta_idx` (`cod_ruta`),
-  CONSTRAINT `pasajero_reserva_pasajero` FOREIGN KEY (`dni`) REFERENCES `pasajero` (`dni`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `pasajero_reserva_reserva` FOREIGN KEY (`fecha_res`) REFERENCES `reserva` (`fecha_res`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `pasajero_reserva_ruta` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `pasajero_reserva_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `asiento` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `pasajero_reserva`
+-- Estructura de tabla para la tabla `plan`
 --
 
-LOCK TABLES `pasajero_reserva` WRITE;
-/*!40000 ALTER TABLE `pasajero_reserva` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pasajero_reserva` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `plan`
---
-
-DROP TABLE IF EXISTS `plan`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plan` (
-  `fecha_hora_plan` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `fecha_hora_plan` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `patente` int(11) NOT NULL,
   `cuil` int(11) NOT NULL,
   `cod_ruta` int(11) NOT NULL,
-  `precio` double default NULL,
-  PRIMARY KEY  (`fecha_hora_plan`,`patente`,`cuil`,`cod_ruta`)
+  `precio` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `plan`
+-- Estructura de tabla para la tabla `reserva`
 --
 
-LOCK TABLES `plan` WRITE;
-/*!40000 ALTER TABLE `plan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `plan` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reserva`
---
-
-DROP TABLE IF EXISTS `reserva`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reserva` (
-  `fecha_res` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `fecha_res` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `cod_ruta` int(11) NOT NULL,
   `usuario` varchar(45) NOT NULL,
-  `cant_pas` int(11) default NULL,
-  `fecha_viaje` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `fecha_venc` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `fecha_canc` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `cod_compania` int(11) default NULL,
-  `nro_tarjeta` int(11) default NULL,
-  `fecha_pago` timestamp NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`fecha_res`,`cod_ruta`,`usuario`),
-  KEY `ruta_reserva_idx` (`cod_ruta`),
-  KEY `usuario_reserva_idx` (`usuario`),
-  KEY `compania_reserva_idx` (`cod_compania`),
-  CONSTRAINT `compania_reserva` FOREIGN KEY (`cod_compania`) REFERENCES `compania_tarjeta` (`cod_compania`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `ruta_reserva` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `usuario_reserva` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `cant_pas` int(11) DEFAULT NULL,
+  `fecha_viaje` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fecha_venc` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fecha_canc` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `cod_compania` int(11) DEFAULT NULL,
+  `nro_tarjeta` int(11) DEFAULT NULL,
+  `fecha_pago` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `reserva`
+-- Estructura de tabla para la tabla `ruta`
 --
 
-LOCK TABLES `reserva` WRITE;
-/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ruta`
---
-
-DROP TABLE IF EXISTS `ruta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ruta` (
-  `cod_ruta` int(11) NOT NULL auto_increment,
-  `dias_sem` varchar(45) default NULL,
-  PRIMARY KEY  (`cod_ruta`)
+  `cod_ruta` int(11) NOT NULL,
+  `dias_sem` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `ruta`
+-- Estructura de tabla para la tabla `terminal`
 --
 
-LOCK TABLES `ruta` WRITE;
-/*!40000 ALTER TABLE `ruta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ruta` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `terminal`
---
-
-DROP TABLE IF EXISTS `terminal`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `terminal` (
-  `cod_terminal` int(11) NOT NULL auto_increment,
-  `cod_postal` int(11) default NULL,
-  PRIMARY KEY  (`cod_terminal`),
-  KEY `terminal_ciudad_idx` (`cod_postal`),
-  CONSTRAINT `terminal_ciudad` FOREIGN KEY (`cod_postal`) REFERENCES `ciudad` (`cod_postal`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `cod_terminal` int(11) NOT NULL,
+  `cod_postal` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `terminal`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-LOCK TABLES `terminal` WRITE;
-/*!40000 ALTER TABLE `terminal` DISABLE KEYS */;
-/*!40000 ALTER TABLE `terminal` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `usuario` varchar(45) NOT NULL,
-  `rol` varchar(45) NOT NULL default 'Cliente',
+  `rol` varchar(45) NOT NULL DEFAULT 'Cliente',
   `email` varchar(45) NOT NULL,
   `contrasenia` varchar(45) NOT NULL,
-  `cuil` int(11) default NULL,
-  PRIMARY KEY  (`usuario`)
+  `cuil` int(11) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT 'activo'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES ('admin','admin','admin@hotmail.com','admin',123456);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `usuario` (`usuario`, `rol`, `email`, `contrasenia`, `cuil`, `estado`) VALUES
+('admin', 'admin', 'admin@hotmail.com', 'admin', 123456, 'activo');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`cod_postal`);
+
+--
+-- Indices de la tabla `colectivo`
+--
+ALTER TABLE `colectivo`
+  ADD PRIMARY KEY (`patente`);
+
+--
+-- Indices de la tabla `compania_tarjeta`
+--
+ALTER TABLE `compania_tarjeta`
+  ADD PRIMARY KEY (`cod_compania`);
+
+--
+-- Indices de la tabla `escala`
+--
+ALTER TABLE `escala`
+  ADD PRIMARY KEY (`cod_ruta`,`orden`),
+  ADD KEY `escala_terminal_idx` (`cod_terminal`);
+
+--
+-- Indices de la tabla `pasajero`
+--
+ALTER TABLE `pasajero`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `pasajero_reserva`
+--
+ALTER TABLE `pasajero_reserva`
+  ADD PRIMARY KEY (`dni`,`cod_ruta`,`fecha_res`,`usuario`),
+  ADD KEY `pasajero_reserva_usuario_idx` (`usuario`),
+  ADD KEY `pasajero_reserva_reserva_idx` (`fecha_res`),
+  ADD KEY `pasajero_reserva_ruta_idx` (`cod_ruta`);
+
+--
+-- Indices de la tabla `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`fecha_hora_plan`,`patente`,`cuil`,`cod_ruta`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`fecha_res`,`cod_ruta`,`usuario`),
+  ADD KEY `ruta_reserva_idx` (`cod_ruta`),
+  ADD KEY `usuario_reserva_idx` (`usuario`),
+  ADD KEY `compania_reserva_idx` (`cod_compania`);
+
+--
+-- Indices de la tabla `ruta`
+--
+ALTER TABLE `ruta`
+  ADD PRIMARY KEY (`cod_ruta`);
+
+--
+-- Indices de la tabla `terminal`
+--
+ALTER TABLE `terminal`
+  ADD PRIMARY KEY (`cod_terminal`),
+  ADD KEY `terminal_ciudad_idx` (`cod_postal`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `compania_tarjeta`
+--
+ALTER TABLE `compania_tarjeta`
+  MODIFY `cod_compania` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ruta`
+--
+ALTER TABLE `ruta`
+  MODIFY `cod_ruta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `terminal`
+--
+ALTER TABLE `terminal`
+  MODIFY `cod_terminal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `escala`
+--
+ALTER TABLE `escala`
+  ADD CONSTRAINT `escala_ruta` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `escala_terminal` FOREIGN KEY (`cod_terminal`) REFERENCES `terminal` (`cod_terminal`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `pasajero_reserva`
+--
+ALTER TABLE `pasajero_reserva`
+  ADD CONSTRAINT `pasajero_reserva_pasajero` FOREIGN KEY (`dni`) REFERENCES `pasajero` (`dni`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pasajero_reserva_reserva` FOREIGN KEY (`fecha_res`) REFERENCES `reserva` (`fecha_res`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pasajero_reserva_ruta` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pasajero_reserva_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `compania_reserva` FOREIGN KEY (`cod_compania`) REFERENCES `compania_tarjeta` (`cod_compania`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ruta_reserva` FOREIGN KEY (`cod_ruta`) REFERENCES `ruta` (`cod_ruta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `usuario_reserva` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `terminal`
+--
+ALTER TABLE `terminal`
+  ADD CONSTRAINT `terminal_ciudad` FOREIGN KEY (`cod_postal`) REFERENCES `ciudad` (`cod_postal`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-20 18:43:45
