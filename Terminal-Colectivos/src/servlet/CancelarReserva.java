25a180c2bebc078ab02320c99b676c8bebbaf28a
gdcac <gdcac@192.168.1.22>
2020-02-11 11:30:27 -0300

    COMMIT FINAL

--- Terminal-Colectivos/src/servlet/CancelarReserva.java
+++ Terminal-Colectivos/src/servlet/CancelarReserva.java
@@ -37,7 +37,7 @@ public class CancelarReserva extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession sesion = request.getSession();
-Usuario usuarioActual = (Usuario) sesion.getAttribute("usuarioActual");
+		Usuario usuarioActual = (Usuario) sesion.getAttribute("usuarioActual");
 		
 		
 		UsuarioLogic usuLog = new UsuarioLogic();
